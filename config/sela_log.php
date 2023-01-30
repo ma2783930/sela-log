4018fb31c8163ee750e195e25413df9beb2b3c1d
m.alizadeh <m.alizadeh@ip-jam.com>
2023-01-30 09:02:55 +0330

    first commit

---
+++ config/sela_log.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'path'                => 'logs/sela',
+    'use_storage'         => true,
+    'logconfig_file_name' => 'logconfig.json',
+    'actionlog_file_name' => 'actionlog.json',
+    'detaillog_file_name' => 'detaillog.json',
+    'identifiers'         => []
+];
